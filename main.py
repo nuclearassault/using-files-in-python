910ceeabb4dacbedca207860885befc7c9d6f2a5
MrJWaring <james.waring@biswashington.org>
2025-01-09 14:20:06 +0000

    First commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Open the text file containing the list of countries
+with open('countries.txt', 'r') as file:
+    # Read all lines from the file and store them in a list
+    countries = file.readlines()
+
+# Remove any extra whitespace (like newline characters) from each line
+countries = [country.strip() for country in countries]
+
+# Get the count of countries
+country_count = len(countries)
+
+# Output the number of countries
+print(f"There are {country_count} countries listed.")
